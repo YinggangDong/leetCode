e4d30a268b48acc67714c09e3d9753683d36ad82
YinggangDong <believerD@aliyun.com>
2020-08-04 15:53:49 +0800

    Solution349-两个数组的交集
    Signed-off-by: YinggangDong <believerD@aliyun.com>

--- src/main/java/cn/dyg/array/Solution349.java
+++ src/main/java/cn/dyg/array/Solution349.java
@@ -4,7 +4,30 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 /**
- * Solution349 类是
+ * Solution349 类是 两个数组的交集
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *  
+ *
+ * 说明：
+ *
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author dongyinggang
  * @date 2020-08-04 15:38
